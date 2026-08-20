75133c76e7219c8834f9fd0938a7aec60dcc8e58
Daniel Gold <danial.gold5@gmail.com>
2026-08-19 18:58:56 -0600

    Update sprite.py

--- sprite.py
+++ sprite.py
@@ -25,6 +25,7 @@ def img(img_string):
     unlit_mask = []
     bg_color_layer=''
     fg_color_layer=''
+    zero_one_string = ''
     lines = [x.strip() for x in img_string.split('\n')]
     for line in lines:
         
@@ -40,19 +41,23 @@ def img(img_string):
                     lit_up_line.append('.')
                     unlit_line.append('.')
                     bg_color_layer += '1'
+                    #1 in bg is transparent
                     fg_color_layer += '0'
                     #0's in fg image are transparent
                 elif char in ['f']: #f => off
                     new_img_line.append('f')
                     unlit_line.append('0')
                     lit_up_line.append('.')
-                    bg_color_layer += '1'
-                    fg_color_layer += '1'
+                    bg_color_layer += '0'
+                    #1 in bg is transparent
+                    fg_color_layer += '0'
+                    #0's in fg image are transparent
                 elif char in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd','e']:
                     new_img_line.append('9')
                     lit_up_line.append('1')       #9 => lit up, default all others for now
                     unlit_line.append('.')
                     bg_color_layer += '1'
+                    #1 in bg is transparent
                     fg_color_layer += '1'
             new_img.append(new_img_line)
             lit_up_mask.append(lit_up_line)
@@ -63,8 +68,15 @@ def img(img_string):
             zero_one_string+=char
     print(zero_one_string)#Draw bg color layer down first.  1's in the pbm show up as black on gimp.  Those ultimately are transparent in the final sprite
     
-    b=bytearray(int(zero_one_string, 2).to_bytes((len(zero_one_string) + 7) // 8, 'big'))
-    print(b)
+    #b=bytearray(int(zero_one_string, 2).to_bytes((len(zero_one_string) + 7) // 8, 'big'))
+    #print(b)
+    print()
+    bg_color_image = bytearray(int(bg_color_layer, 2).to_bytes((len(bg_color_layer) + 7) // 8, 'big'))
+    print(bg_color_layer)
+    print(bg_color_image)
+    print()
+    fg_color_image = bytearray(int(fg_color_layer, 2).to_bytes((len(fg_color_layer) + 7) // 8, 'big'))
+    print(fg_color_layer)
     #f is black, 9 is light blue
 a = img("""
         . . . . . . . . . . . . . . . .
